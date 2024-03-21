4c717024dccf6346d31cf400a76ebdba914bdedd
Angel Sigueros <pickassblog@gmail.com>
2024-03-21 21:17:43 +0100

    imagen en author form

--- fullstack-001/frontend/src/app/author-form/author-form.component.ts
+++ fullstack-001/frontend/src/app/author-form/author-form.component.ts
@@ -12,15 +12,36 @@ import { RouterLink } from '@angular/router';
 })
 export class AuthorFormComponent implements OnInit {
   
-  onFileChange($event: Event) {
-    
-  }
-
+  photoFile: File | undefined;
+  photoPreview: string | undefined;
   authorForm = new FormGroup({
     fullName: new FormControl(''),
   });
 
+  
   constructor(private http: HttpClient) {}
 
+  
   ngOnInit(): void {}
+
+  
+  onFileChange(event: Event) {
+    console.log(event);
+    
+    let target = event.target as HTMLInputElement; 
+    if (target.files === null || target.files.length == 0)
+      return;
+    
+    console.log(target.files[0]);
+
+    this.photoFile = target.files[0];
+
+    // mostrar img
+    let reader = new FileReader();
+    reader.onload = event => this.photoPreview = reader.result as string;
+    reader.readAsDataURL(this.photoFile);
+
+  }
+
+  save(){}
 }
