544ed24de29d1eb55d0db455eacdaa96997c054f
Angel Sigueros <pickassblog@gmail.com>
2024-03-21 19:03:20 +0100

    reserva y author forms

--- fullstack-001/frontend/src/app/author-form/author-form.component.ts
+++ fullstack-001/frontend/src/app/author-form/author-form.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-author-form',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, ReactiveFormsModule, RouterLink],
   templateUrl: './author-form.component.html',
-  styleUrl: './author-form.component.css'
+  styleUrl: './author-form.component.css',
 })
-export class AuthorFormComponent {
+export class AuthorFormComponent implements OnInit {
+  
+  onFileChange($event: Event) {
+    
+  }
 
+  authorForm = new FormGroup({
+    fullName: new FormControl(''),
+  });
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
 }
