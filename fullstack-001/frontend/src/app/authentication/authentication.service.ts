0ce30ea9f14a9ddca82d5128d395e476c36e4b5f
Angel <pickassblog@gmail.com>
2024-04-04 16:11:51 +0200

    logIn LogOut

--- fullstack-001/frontend/src/app/authentication/authentication.service.ts
+++ fullstack-001/frontend/src/app/authentication/authentication.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
+  isLoggedIn = new BehaviorSubject<boolean>(this.existToken());
+
   constructor() { }
+
+  existToken() {
+    return localStorage.getItem("jwt_token") != null;
+  }
 
   saveToken(token: string) {
     localStorage.setItem("jwt_token", token);
+    this.isLoggedIn.next(true);
   }
 }
